de48c755700557406d534f9ad2752a528106d538
shahid-shafi <shahidshafi.org@gmail.com>
2023-09-02 20:11:55 +0530

    connect mongodb db, add type defs

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,26 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
 import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
+import typeDefs from './graphql/typeDefs';
+import resolvers from './graphql/resolvers';
+import mongoose, { ConnectOptions } from 'mongoose';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { typeDefs } from './schema';
 
+const dbUrl = 'mongodb://127.0.0.1:27017/graphql';
 
-const products = [
-    {
-        title: 'Iphone 15 Pro Max',
-        price: 1500,
-        quantity: 10,
-    },
-    {
-        title: 'OnePlus Nord 3',
-        price: 500,
-        quantity: 10,
-    },
-];
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+const PORT = 8000;
 
-const resolvers = {
-    Query: {
-        getProducts: () => products,
-    },
-};
-
-const startServer = async () => {
-    const app = express();
-    const server = new ApolloServer({
-        typeDefs,
-        resolvers,
-    });
-
-    app.use(bodyParser.json());
-    app.use(express.json());
-    app.use(cors());
-    await server.start()
-    app.use('/graphql', expressMiddleware(server));
-
-    const PORT = 8000;
-    app.listen(PORT, () => console.log('Server listening on port: ', PORT))
-}
-
-startServer();
+mongoose
+    .connect(dbUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        autoIndex: true,
+    } as ConnectOptions)
+    .then(async () => {
+        console.log('✅ DataBase Connected Successfully...');
+        const { url } = await startStandaloneServer(server);
+        return url;
+    }).then(url => console.log(`🚀 Server running at ${url}`))
+    .catch((error) => console.log('❌ Error:', error));
